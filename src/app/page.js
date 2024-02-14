4b23283ba5686d6f84052c226fe8b58d944140a5
mask-shakill <mask-shakill>
2024-02-14 19:18:57 +0600

    added Balance

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,22 @@
+import Balance from "@/components/Available-Balance/Balance";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="mx-36 mt-10">
-      <div className="h-[350px] w-full bg-green-900 rounded-md grid grid-cols-5">
-        <div className="bg-red-400 col-span-2 flex flex-col justify-between">
-          <div className="bg-green-500">div one</div>
-          <div className="bg-indigo-500">div one</div>
+      <section className="h-[300px] w-full bg-green-950 rounded-lg grid grid-cols-5">
+        <div className="col-span-2 flex flex-col justify-around ml-20">
+          <Balance />
+          <Balance />
         </div>
-        <div className="bg-blue-400 col-span-1 flex items-center justify-center">
-          div 1
+        <div className="col-span-1 flex items-center justify-center">
+          <Balance />
         </div>
-        {/* sa  */}
-        <div className="bg-yellow-400 col-span-2 flex flex-col justify-between">
-          <div className="bg-green-500">div one</div>
-          <div className="bg-indigo-500">div one</div>
+        <div className="col-span-2 flex flex-col justify-around items-end mr-20">
+          <Balance />
+          <Balance />
         </div>
-      </div>
+      </section>
     </div>
   );
 };
---
+++ src/components/Available-Balance/Balance.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Balance = () => {
+  return (
+    <div className="text-white ">
+      <h1 className="font-bold text-3xl">Available Balance</h1>
+      <p className="font-semibold mt-2 ">Tk. 25000.00</p>
+    </div>
+  );
+};
+
+export default Balance;
